8befd09cb56a6544e35e374cf787b85c397eb37c
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-20 13:45:51 +0300

    All done 100/100 :)

--- Stacks and Queues/2. Stack Sum/Program.cs
+++ Stacks and Queues/2. Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2._Stack_Sum
 {
@@ -6,7 +8,38 @@ namespace _2._Stack_Sum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] elements = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            var stack = new Stack<int>(elements);
+
+            string command;
+            while ((command = Console.ReadLine().ToLower()) != "end")
+            {
+
+                var commandTokens = command.Split(' ');
+                if (commandTokens[0] == "add")
+                {
+                    int n1 = int.Parse(commandTokens[1]);
+                    int n2 = int.Parse(commandTokens[2]);
+                    stack.Push(n1);
+                    stack.Push(n2);
+                }
+                else if (commandTokens[0] == "remove")
+                {
+                    int count = int.Parse(commandTokens[1]);
+                    if (stack.Count >= count)
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Sum: {stack.Sum()}");
+
         }
     }
 }
