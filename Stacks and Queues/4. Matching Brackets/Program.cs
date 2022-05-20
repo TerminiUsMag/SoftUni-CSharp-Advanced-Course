5aba371302dd7d3571c62f3f4a9360a94b47e026
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-20 19:18:45 +0300

    All done 100/100 :)

--- Stacks and Queues/4. Matching Brackets/Program.cs
+++ Stacks and Queues/4. Matching Brackets/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace _4._Matching_Brackets
 {
@@ -6,7 +9,28 @@ namespace _4._Matching_Brackets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stack = new Stack<int>();
+
+            string expression = Console.ReadLine();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char c = expression[i];
+
+                if (c == '(')
+                {
+                    stack.Push(i);
+                }
+                if (c == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+                    string subExpression = expression.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+
+
         }
     }
 }
