549983dd55803ac36a37713fc1d0b0a6ea922b98
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-20 20:56:34 +0300

    08. All done 100/100 :) :)

--- Stacks and Queues/8. Traffic Jam/Program.cs
+++ Stacks and Queues/8. Traffic Jam/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace _8._Traffic_Jam
 {
@@ -6,7 +9,33 @@ namespace _8._Traffic_Jam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>();
+
+            int carsPassed = 0;
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "green")
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        if (1 <= queue.Count)
+                        {
+                            Console.WriteLine($"{queue.Dequeue()} passed!");
+                            carsPassed++;
+                        }
+                    }
+                }
+                else if (command == "end") break;
+                else
+                {
+                    queue.Enqueue(command);
+                }
+            }
+            Console.WriteLine($"{carsPassed} cars passed the crossroads.");
         }
     }
 }
