ea2209f05c530dc6b7149261ae67042548676081
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-25 20:52:03 +0300

    02.All done 100/100 :)

---
+++ Multidimensional Arrays - Lab/02.Sum Matrix Columns/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[n[0], n[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] values = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int sumCol = 0;
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    sumCol += matrix[j, i];
+                }
+                Console.WriteLine(sumCol);
+            }
+        }
+    }
+}
