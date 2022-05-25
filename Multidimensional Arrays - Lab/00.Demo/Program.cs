4c6d91f04a14f5b0156ada882d139f6719f562bb
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-25 20:45:05 +0300

    01. All done 100/100 :)

--- Multidimensional Arrays - Lab/00.Demo/Program.cs
+++ Multidimensional Arrays - Lab/00.Demo/Program.cs
@@ -6,7 +6,32 @@ namespace _00.Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] matrix = new int[5, 5];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            PrintMatrix(matrix);
+
+            matrix[0, 4] = 10;
+
+        }
+        static void PrintMatrix(int[,] matrixToPrint)
+        {
+            for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+                {
+                    Console.Write(matrixToPrint[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- Multidimensional Arrays - Lab/01.Sum Matrix Elements/Program.cs
+++ Multidimensional Arrays - Lab/01.Sum Matrix Elements/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01.Sum_Matrix_Elements
 {
@@ -6,7 +7,24 @@ namespace _01.Sum_Matrix_Elements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] n = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int[,] matrix = new int[n[0], n[1]];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] values = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+            int sum = 0;
+            foreach (var el in matrix)
+            {
+                sum += el;
+            }
+            Console.WriteLine($"{n[0]}\n{n[1]}\n{sum}");
         }
     }
 }
