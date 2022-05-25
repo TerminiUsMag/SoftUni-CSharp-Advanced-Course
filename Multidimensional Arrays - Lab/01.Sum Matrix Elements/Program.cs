d7ce019cd8953c44aa6b877fc06a1b9502006e4e
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-25 20:07:56 +0300

    New Lecture

---
+++ Multidimensional Arrays - Lab/01.Sum Matrix Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
