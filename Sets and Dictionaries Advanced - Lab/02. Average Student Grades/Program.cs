501eac38b6f470802e4afd75be1f72c5ec20ce1a
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-28 21:27:06 +0300

    Just the formating of the grades left :)

---
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02._Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var dict = new Dictionary<string, List<double>>();
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                string[] commandTokens = command.Split(' ');
+
+                string nameOfStudent = commandTokens[0];
+                double grade = double.Parse(commandTokens[1]);
+
+                if (!dict.ContainsKey(nameOfStudent))
+                {
+                    var newList = new List<double>();
+                    dict.Add(nameOfStudent, newList);
+                }
+                dict[nameOfStudent].Add(grade);
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {string.Join(' ', item.Value):f3} (avg: {item.Value.Average():f2})");
+            }
+        }
+    }
+}
