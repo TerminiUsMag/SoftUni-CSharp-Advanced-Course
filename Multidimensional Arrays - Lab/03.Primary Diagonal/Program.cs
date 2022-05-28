6c4d101cc516dc89cd4ca39e7071d16b1137430e
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-28 16:23:52 +0300

    Added 04. | All done on 03. 100/100 | Changed properties for startup selection :)

--- Multidimensional Arrays - Lab/03.Primary Diagonal/Program.cs
+++ Multidimensional Arrays - Lab/03.Primary Diagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _03.Primary_Diagonal
 {
@@ -6,7 +7,25 @@ namespace _03.Primary_Diagonal
     {
         static void Main(string[] args)
         {
-            
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+            long diagonalSum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] values = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = values[j];
+                    if (i == j)
+                    {
+                        diagonalSum += matrix[i, j];
+                    }
+                }
+            }
+
+            Console.WriteLine(diagonalSum);
+
+
         }
     }
 }
---
+++ Multidimensional Arrays - Lab/04.Symbol in Matrix/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Symbol_in_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string characters = Console.ReadLine();
+                foreach (char c in characters)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        matrix[i, j] = c;
+                    }
+                }
+            }
+
+            char toFind = char.Parse(Console.ReadLine());
+            bool found = false;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i, j] == toFind)
+                    {
+                        Console.WriteLine($"({i}, {j})");
+                        found = true;
+                    }
+                }
+                if (found) break;
+            }
+
+            if (!found)
+            {
+                Console.WriteLine($"{toFind} does not occur in the matrix");
+            }
+
+        }
+    }
+}
