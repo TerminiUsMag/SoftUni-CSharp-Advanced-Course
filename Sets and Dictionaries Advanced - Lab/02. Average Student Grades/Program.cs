4b98bb966af451fb16735602257bddb1a17b0890
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-29 18:35:12 +0300

    02.All done 100/100 :)

--- Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
+++ Sets and Dictionaries Advanced - Lab/02. Average Student Grades/Program.cs
@@ -1,35 +1,30 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _02._Average_Student_Grades
+class AverageStudentGrades
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        var studentGrades = new Dictionary<string, List<decimal>>();
+        int gradesCount = int.Parse(Console.ReadLine());
+        for (int i = 0; i < gradesCount; i++)
         {
-            int n = int.Parse(Console.ReadLine());
-            var dict = new Dictionary<string, List<double>>();
-            for (int i = 0; i < n; i++)
-            {
-                string command = Console.ReadLine();
-                string[] commandTokens = command.Split(' ');
+            string[] line = Console.ReadLine().Split(' ');
+            string name = line[0];
+            decimal grade = decimal.Parse(line[1]);
+            if (!studentGrades.ContainsKey(name))
+                studentGrades.Add(name, new List<decimal>());
+            studentGrades[name].Add(grade);
+        }
 
-                string nameOfStudent = commandTokens[0];
-                double grade = double.Parse(commandTokens[1]);
-
-                if (!dict.ContainsKey(nameOfStudent))
-                {
-                    var newList = new List<double>();
-                    dict.Add(nameOfStudent, newList);
-                }
-                dict[nameOfStudent].Add(grade);
-            }
-
-            foreach (var item in dict)
-            {
-                Console.WriteLine($"{item.Key} -> {string.Join(' ', item.Value):f3} (avg: {item.Value.Average():f2})");
-            }
+        foreach (var name in studentGrades.Keys)
+        {
+            List<decimal> grades = studentGrades[name];
+            string gradesStr = string.Join(" ",
+                grades.Select(g => g.ToString("f2")));
+            decimal avg = grades.Average();
+            Console.WriteLine($"{name} -> {gradesStr} (avg: {avg:f2})");
         }
     }
 }
