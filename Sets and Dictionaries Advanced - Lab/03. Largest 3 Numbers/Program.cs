358e57182ba0ad8f48295a77fc098cc6e50f3812
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-29 18:46:29 +0300

    03. All done 100/100 :)

---
+++ Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            list = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).OrderByDescending(x => x).ToList();
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (i < list.Count)
+                {
+                    Console.Write(list[i] + " ");
+                }
+            }
+
+        }
+    }
+}
