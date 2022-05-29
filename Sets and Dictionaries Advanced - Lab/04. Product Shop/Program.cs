23253570f4040e674b4dfc96e36b00b813ff7310
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-29 19:44:55 +0300

    05.All done 100/100 :) first try :):)

---
+++ Sets and Dictionaries Advanced - Lab/04. Product Shop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _04._Product_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var shops = new Dictionary<string, Dictionary<string, double>>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Revision")
+            {
+                string[] inputs = cmd.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string shop = inputs[0];
+                string product = inputs[1];
+                double price = double.Parse(inputs[2]);
+
+                if (!shops.ContainsKey(shop))
+                {
+                    var newProductsDict = new Dictionary<string, double>();
+                    shops.Add(shop, newProductsDict);
+                }
+                if (!shops[shop].ContainsKey(product))
+                {
+                    shops[shop].Add(product, price);
+                }
+                else
+                {
+                    shops[shop][product] = price;
+                }
+            }
+
+            var orderedShops = shops.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach(var shop in orderedShops)
+            {
+                Console.WriteLine($"{shop.Key}->");
+                foreach(var product in shop.Value)
+                {
+                    Console.WriteLine($"Product: {product.Key}, Price: {product.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0;i<n; i++)
+            {
+                string[] inputs = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string continent = inputs[0];
+                string country = inputs[1];
+                string city = inputs[2];
+
+                if (!map.ContainsKey(continent))
+                {
+                    var newCountryDict = new Dictionary<string, List<string>>();
+                    map.Add(continent, newCountryDict);
+                }
+
+                if (!map[continent].ContainsKey(country))
+                {
+                    var newCitiesList = new List<string>();
+                    map[continent].Add(country, newCitiesList);
+                }
+
+                map[continent][country].Add(city); 
+            }
+            foreach(var continent in map)
+            {
+                Console.WriteLine($"{continent.Key}:");
+                foreach(var country in continent.Value)
+                {
+                    Console.WriteLine($"{country.Key} -> {string.Join(", ",country.Value)}");
+                }
+            }
+
+
+
+        }
+    }
+}
