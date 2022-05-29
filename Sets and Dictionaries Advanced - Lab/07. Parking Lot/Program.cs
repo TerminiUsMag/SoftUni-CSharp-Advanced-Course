01f280997c0283166ee88f2b08af9aae2c8714cc
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-29 20:37:34 +0300

    07.All done 100/100

---
+++ Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var parkingLot = new HashSet<string>();
+            string command;
+
+            while ((command = Console.ReadLine())!="END")
+            {
+                string[] cmd = command.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string operation = cmd[0];
+                string plate = cmd[1];
+
+                if (operation == "IN")
+                    parkingLot.Add(plate);
+
+                if (operation == "OUT")
+                    parkingLot.Remove(plate);
+
+
+            }
+
+            if(parkingLot.Count > 0)
+            {
+                foreach(var car in parkingLot)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
+        }
+    }
+}
