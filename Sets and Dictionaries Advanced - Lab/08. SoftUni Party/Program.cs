011fef2998a1470cdcbc7334ba507c6cfd3d0313
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-30 18:03:58 +0300

    08.All done 100/100

---
+++ Sets and Dictionaries Advanced - Lab/08. SoftUni Party/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _08._SoftUni_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vips = new HashSet<string>();
+            var regulars = new HashSet<string>();
+
+            string cmd;
+
+            while ((cmd = Console.ReadLine()) != "PARTY")
+            {
+                char vipOrNot = cmd[0];
+                if (char.IsDigit(vipOrNot))
+                {
+                    vips.Add(cmd);
+                }
+                else
+                {
+                    regulars.Add(cmd);
+                }
+            }
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                vips.Remove(cmd);
+                regulars.Remove(cmd);
+            }
+
+            Console.WriteLine(vips.Count + regulars.Count);
+            foreach (var vip in vips)
+            {
+                Console.WriteLine(vip);
+            }
+            foreach (var regular in regulars)
+            {
+                Console.WriteLine(regular);
+            }
+        }
+    }
+}
