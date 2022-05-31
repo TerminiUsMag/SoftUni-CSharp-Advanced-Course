ffcec35cb2dacef2b54eff8d7684b4f91961f70e
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-31 23:43:48 +0300

    Added skeleton for 04. Merge Text Files

---
+++ Streams, Files and Directories - Lab/04. Merge Text Files/MergeFiles.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            // TODO: write your code here…
+        }
+    }
+}
+
