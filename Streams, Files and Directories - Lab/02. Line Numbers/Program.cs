c1c60441ed0f21e341566d27537e30f3da09f1b2
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-31 22:57:04 +0300

    02.All done 100/100 (.NET Core 3.1)

---
+++ Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                var writer = new StreamWriter(outputFilePath);
+                using (writer)
+                {
+                    string line;
+                    int lineNum = 1;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        writer.WriteLine($"{lineNum}. {line}");
+                        lineNum++;
+                    }
+                }
+            }
+        }
+    }
+}
