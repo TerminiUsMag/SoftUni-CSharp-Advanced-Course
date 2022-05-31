c705fee4db0fc085019454a7f5a8e3705ef0e8e1
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-05-31 23:39:50 +0300

    03. Done

---
+++ Streams, Files and Directories - Lab/03. Word Count/WordCount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\words.txt";
+            string textPath = @"..\..\..\text.txt";
+            string outputPath = @"..\..\..\output.txt";
+
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(textFilePath);
+            var wordsToFind = new StreamReader(wordsFilePath);
+            var writer = new StreamWriter(outputFilePath);
+            using (reader)
+            {
+                using (wordsToFind)
+                {
+                    using (writer)
+                    {
+                        var separators = new string[] { " ", ",", ".", "!", "?", "\r", "\n","-"};
+                        var wordsDict = wordsToFind
+                            .ReadToEnd()
+                            .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(x => x.ToLower())
+                            .Select((value, index) => new { value, index = 0 })
+                            .ToDictionary(pair => pair.value, pair => pair.index);
+
+                        string[] textInWords = reader
+                            .ReadToEnd()
+                            .Split(separators, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(x => x.ToLower())
+                            .ToArray();
+
+                        for (int i = 0; i < textInWords.Length; i++)
+                        {
+                            if (wordsDict.ContainsKey(textInWords[i]))
+                            {
+                                wordsDict[textInWords[i]]++;
+                            }
+                        }
+
+                        var sortedDict = wordsDict.OrderByDescending(x => x.Value);
+
+                        foreach (var (word, ocurr) in sortedDict)
+                        {
+                            writer.WriteLine($"{word} - {ocurr}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
