7acad01f1f37e6b25597b4ab1b131a42d7cd88e6
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-08 12:55:35 +0300

    IDK

--- Streams, Files and Directories - Lab/05. Extract Special Bytes/ExtractSpecialBytes.cs
+++ Streams, Files and Directories - Lab/05. Extract Special Bytes/ExtractSpecialBytes.cs
@@ -19,7 +19,34 @@ namespace ExtractSpecialBytes
 
         public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
         {
-            // TODO: write your code here…
+            using (var reader = new FileStream(binaryFilePath, FileMode.Open, FileAccess.Read))
+            {
+                using(var bytesReader = new StreamReader(bytesFilePath))
+                {
+                    using(var writer = new FileStream(outputPath, FileMode.Create, FileAccess.Write))
+                    {
+                        
+                        var bytes = File.ReadAllBytes(bytesFilePath);
+                        var binaryFileBytes = File.ReadAllBytes(binaryFilePath);
+                        byte[] output = new byte[binaryFileBytes.Length];
+                        int counter = 0;
+                        for(int i = 0; i < bytes.Length; i++)
+                        {
+                            for(int j =0; j < binaryFileBytes.Length; j++)
+                            {
+                                if(bytes[i] == binaryFileBytes[j])
+                                {
+                                    output[counter]=bytes[i];
+                                    counter++;
+                                }
+                            }
+                        }
+
+                        writer.Write(output);
+                        
+                    }
+                }
+            }
         }
     }
 }
