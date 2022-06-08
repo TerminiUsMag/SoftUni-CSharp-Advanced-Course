68777ff0392cd1ef3271e30bce70ada06303d899
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-08 21:46:32 +0300

    03. Almost done

---
+++ Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stak = new Stack<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] cmd = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                switch (cmd[0])
+                {
+                    case 1:
+                        int x = cmd[1];
+                        stak.Push(x);
+                        break;
+                    case 2:
+                        stak.Pop();
+                        break;
+                    case 3:
+                        if(stak.Count>0)
+                            Console.WriteLine(stak.Max());
+                        break;
+                    case 4:
+                        if (stak.Count > 0)
+                            Console.WriteLine(stak.Max());
+                        break;
+                }
+            }
+            Console.WriteLine(String.Join(stak.ToString()));
+        }
+    }
+}
