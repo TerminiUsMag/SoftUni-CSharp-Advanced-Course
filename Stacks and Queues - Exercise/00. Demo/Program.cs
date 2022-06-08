da2a6e9c9cd8b9fc75ddd1400da54782909202fc
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-08 21:25:25 +0300

    02. All done 100/100 :)

--- Stacks and Queues - Exercise/00. Demo/Program.cs
+++ Stacks and Queues - Exercise/00. Demo/Program.cs
@@ -8,41 +8,7 @@ namespace _00._Demo
     {
         static void Main(string[] args)
         {
-            var stak = new Stack<int>();
-            int[] input = Console.ReadLine()
-                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-
-            int n = input[0];
-            int s = input[1];
-            int x = input[2];
-
-            input = Console.ReadLine()
-                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToArray();
-
-            for (int i = 0; i < n; i++)
-            {
-                stak.Push(input[i]);
-            }
-            for (int j = 0; j < s; j++)
-            {
-                stak.Pop();
-            }
-            if (stak.Count == 0)
-            {
-                Console.WriteLine(0);
-            }
-            else if (stak.Contains(x))
-            {
-                Console.WriteLine("true");
-            }
-            else
-            {
-                Console.WriteLine(stak.Min()); 
-            }
+            
         }
     }
 }
---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new Queue<int>();
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(input[i]);
+            }
+            for (int j = 0; j < s; j++)
+            {
+                queue.Dequeue();
+            }
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
