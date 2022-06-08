60fa7c9af49c0f43e341808d626cd96c5dff0dc3
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-08 14:41:17 +0300

    Adding some demos to understand the examples

---
+++ Functional Programming - Lab/00. Demos/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Func<int, int> twice = x => x * x;
+
+var func =
+    () => 
+    { 
+        Console.WriteLine("Hello !");
+        Console.WriteLine("I am an annonymous function !!");
+    };
+
+func();
+
+Console.WriteLine();
+
+func = Print;
+
+func();
+
+void Print()
+{
+    Console.WriteLine("Hello, I am Print() !");
+    Console.WriteLine("And I am named function !!");
+}
