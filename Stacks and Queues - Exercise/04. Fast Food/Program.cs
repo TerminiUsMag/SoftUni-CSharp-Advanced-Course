5eb22953d3986cb180b5b3d5e91e3864750d8e69
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-09 22:18:34 +0300

    04. All Done 100/100 :)

--- Stacks and Queues - Exercise/04. Fast Food/Program.cs
+++ Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _04._Fast_Food
 {
@@ -6,7 +8,46 @@ namespace _04._Fast_Food
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var orders = new Queue<int>();
+
+            int food = int.Parse(Console.ReadLine());
+
+            int[] order = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < order.Length; i++)
+            {
+                orders.Enqueue(order[i]);
+            }
+
+            Console.WriteLine(orders.Max());
+            int numberOfOrders = orders.Count;
+
+            for (int j = 0; j < numberOfOrders; j++)
+            {
+                int currOrder = orders.Peek();
+
+                if (food >= currOrder)
+                {
+                    food -= currOrder;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (orders.Count != 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
         }
     }
 }
