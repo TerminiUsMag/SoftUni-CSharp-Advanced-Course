2267b278a8b4170232ab9e0a78443c1d8a5b3148
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-09 22:53:14 +0300

    Added explanations to the example

--- Stacks and Queues - Exercise/04. Fast Food/Program.cs
+++ Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -8,23 +8,23 @@ namespace _04._Fast_Food
     {
         static void Main(string[] args)
         {
+            //Declaring variables.
             var orders = new Queue<int>();
-
             int food = int.Parse(Console.ReadLine());
-
             int[] order = Console.ReadLine()
                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
-
+            //Getting the orders inside the Queue
             for (int i = 0; i < order.Length; i++)
             {
                 orders.Enqueue(order[i]);
             }
-
+            //Printing the biggest order.
             Console.WriteLine(orders.Max());
+            //Getting the number of all orders to be static for the FOR cicle.
             int numberOfOrders = orders.Count;
-
+            //The FOR cicle used for the logic of the example.
             for (int j = 0; j < numberOfOrders; j++)
             {
                 int currOrder = orders.Peek();
@@ -39,7 +39,7 @@ namespace _04._Fast_Food
                     break;
                 }
             }
-
+            //Printing the result.
             if (orders.Count != 0)
             {
                 Console.WriteLine($"Orders left: {string.Join(' ', orders)}");
