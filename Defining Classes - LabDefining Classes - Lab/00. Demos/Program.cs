e97148fdb256334e0962c7c0c1c404105885080c
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-13 17:35:05 +0300

    Some demos in .Net 6.0

---
+++ Defining Classes - LabDefining Classes - Lab/00. Demos/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Dice d6 = new Dice();
+d6.Sides = 6;
+d6.Type = "Plastic Classsical Dice";
+var d8 = new Dice();
+d8.Sides = 8;
+d8.Type = "Wooden Fast Dice";
+Console.WriteLine($"d6 is {d6.Type} with {d6.Sides} sides.");
+Console.WriteLine($"d8 is {d8.Type} with {d8.Sides} sides.");
+
+
+class Dice
+{
+    int sides;   //field
+    string type; //field
+
+    public int Sides //property
+    {
+        get { return sides; }   //getter (get value)
+        set { sides = value; }  //setter (assign value)
+    }
+    public string Type
+    {
+        get { return type; }
+        set { type = value; }
+    }
+}
