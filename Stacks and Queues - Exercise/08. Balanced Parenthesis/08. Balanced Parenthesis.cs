d86a6a4f2f551ba9cd6f97b339cffca9e60b5742
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-13 14:39:03 +0300

    01. All Done 100/100 :)

--- Stacks and Queues - Exercise/08. Balanced Parenthesis/08. Balanced Parenthesis.cs
+++ Stacks and Queues - Exercise/08. Balanced Parenthesis/08. Balanced Parenthesis.cs
@@ -8,8 +8,23 @@ namespace _08._Balanced_Parenthesis
     {
         static void Main(string[] args)
         {
+            var stak = new Stack<char>();
 
+            char[] input = Console.ReadLine().ToCharArray();
 
+            for(int i = 0; i < input.Length; i++)
+            {
+                stak.Push(input[i]);
+            }
+            char lastCh = stak.Pop();
+            char currCh = ' ';
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (i != 0) lastCh = currCh;
+
+               currCh = stak.Pop();
+
+            }
         }
     }
 }
