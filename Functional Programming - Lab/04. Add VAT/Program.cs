ec5f6cb7f808411d40ae9cda116f1de3532d4ee9
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-13 15:54:35 +0300

    04. All done 100/100 :)

---
+++ Functional Programming - Lab/04. Add VAT/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine(string.Join('\n',Console.ReadLine()
+                .Split(", ",StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .Select(x=>x*1.20m)
+                .Select(x=>Math.Round(x,2)
+                ))
+                );
+        }
+    }
+}
