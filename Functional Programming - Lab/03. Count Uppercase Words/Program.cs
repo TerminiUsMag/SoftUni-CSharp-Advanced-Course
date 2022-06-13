9336cf35163855aaaeb00ac688a336b67b451e5f
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-13 15:01:10 +0300

    03. All done using a function 100/100 :)

---
+++ Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> upperCase =
+                x => char.IsUpper(x[0]);
+
+            Console.WriteLine(string.Join('\n',Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Where(upperCase)
+                ));
+        }
+    }
+}
