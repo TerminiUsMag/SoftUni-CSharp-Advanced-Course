49750263e383acf32a62b23b65f5c820305d4d07
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-15 15:47:08 +0300

    01. All done 100/100 :)

---
+++ Defining Classes - LabDefining Classes - Lab/01. Car/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    class Car
+    {
+        // TODO: define the Car class members here …
+        int year;
+        string make;
+        string model;
+
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+    }
+}
+
