f112286086b8adedb1198e9cb726ab257a5c4b23
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-15 16:57:27 +0300

    Created 05. and added Car, Engine, Tire classes :)

---
+++ Defining Classes - LabDefining Classes - Lab/05. Special Cars/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string cmd;
+            while((cmd = Console.ReadLine())!="No more tires")
+            {
+                var tires = new Tire[]
+            }
+        }
+    }
+}
