a999d01ac23c15b31d32458b22bf5a7e7bca69bd
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-15 18:41:26 +0300

    All DONE 100/100 Oficially

---
+++ Defining Classes - LabDefining Classes - Lab/05. Special Cars/Special Cars/Car.cs
@@ -0,0 +1,92 @@
+﻿using System;
+namespace Special_Cars
+{
+    public class Car
+    {
+        private int year;
+        private string make;
+        private string model;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Tire[] tires;
+        private Engine engine;
+        public Car()
+        {
+            this.Make = "VW";
+            this.Model = "Golf";
+            this.Year = 2025;
+            this.FuelQuantity = 200;
+            this.FuelConsumption = 10;
+        }
+        public Car(string make, string model, int year)
+        : this()
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        : this(make, model, year)
+        {
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires)
+        : this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+        public Engine Engine
+        {
+            get { return engine; }
+            set { engine = value; }
+        }
+        public Tire[] Tires
+        {
+            get { return tires; }
+            set { tires = value; }
+        }
+        public double FuelConsumption
+        {
+            get { return fuelConsumption; }
+            set { fuelConsumption = value; }
+        }
+        public int Year { get; set; }
+        public string Model { get; set; }
+        public string Make { get; set; }
+
+        public double FuelQuantity
+        {
+            get { return fuelQuantity; }
+            set { fuelQuantity = value; }
+        }
+
+        public void Drive(double distance)
+        {
+            // x /100 * fuelConsumpt - fuel needed for x distance;
+            double fuelNeededForDistance = (distance / 100) * this.FuelConsumption;
+            if (fuelNeededForDistance <= FuelQuantity)
+            {
+                FuelQuantity -= fuelNeededForDistance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+        public double PressureInTires()
+        {
+            double pressureInTires = 0;
+            foreach (var tire in this.Tires)
+            {
+                pressureInTires += tire.Pressure;
+            }
+            return pressureInTires;
+        }
+    }
+}
---
+++ Defining Classes - LabDefining Classes - Lab/05. Special Cars/Special Cars/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Special_Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              var allTirePairs = new List<Tire[]>();
+            var allEngines = new List<Engine>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "No more tires")
+            {
+                var tiresInfo = cmd
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+
+                var tires = new Tire[4]{
+                    new Tire((int)tiresInfo[0], tiresInfo[1]),
+                    new Tire((int)tiresInfo[2], tiresInfo[3]),
+                    new Tire((int)tiresInfo[4], tiresInfo[5]),
+                    new Tire((int)tiresInfo[6], tiresInfo[7]),
+                };
+                allTirePairs.Add(tires);
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "Engines done")
+            {
+                var enginesInfo = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+                var engine = new Engine((int)enginesInfo[0], enginesInfo[1]);
+                allEngines.Add(engine);
+            }
+            var cars = new List<Car>();
+            var specialCars = new List<Car>();
+            while ((cmd = Console.ReadLine()) != "Show special")
+            {
+                var tokens = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string make = tokens[0];
+                string model = tokens[1];
+                int year = int.Parse(tokens[2]);
+                double fuelQuantity = double.Parse(tokens[3]);
+                double fuelConsumption = double.Parse(tokens[4]);
+                int engineIndex = int.Parse(tokens[5]);
+                int tireIndex = int.Parse(tokens[6]);
+                var tiresForVehicle = allTirePairs[tireIndex];
+                var engineForVehicle = allEngines[engineIndex];
+
+                var newCar = new Car(make, model, year, fuelQuantity, fuelConsumption, engineForVehicle, tiresForVehicle);
+                cars.Add(newCar);
+            }
+            foreach (var car in cars)
+            {
+                double sumOfTirePressures = car.PressureInTires();
+                if (car.Year >= 2017 && car.Engine.HorsePower > 330 && sumOfTirePressures >= 9 && sumOfTirePressures <= 10)
+                {
+                    car.Drive(20);
+                    specialCars.Add(car);
+                }
+            }
+            foreach (var car in specialCars)
+            {
+                Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}\nHorsePowers: {car.Engine.HorsePower}\nFuelQuantity: {car.FuelQuantity}");
+            }
+        }
+    }
+}
