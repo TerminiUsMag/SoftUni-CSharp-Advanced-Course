d976cf28c58a6aba3c45ead4c9ad9eab0adc81df
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-24 15:45:43 +0300

    Generics-Lab-Examples BoxOfT All done 100/100 :)

---
+++ Generics/BoxOfT/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data;
+        public int Count
+        {
+            get
+            {
+                return data.Count;
+            }
+        }
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+        public T Remove()
+        {
+            var removedElement = this.data[this.data.Count - 1];
+            this.data.RemoveAt(this.data.Count - 1);
+            return removedElement;
+        }
+
+    }
+}
