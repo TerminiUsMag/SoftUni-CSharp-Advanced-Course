92bad014f73a2a519bb10614f7513fff45b7d297
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-24 13:33:29 +0300

    Implemented my own custom list from scratch.

---
+++ Implementing Stack and Queue/Implementing Stack and Queue/MyList.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementing_Stack_and_Queue
+{
+    public class MyList
+    {
+        private int capacity;
+        private const int InitialLenght = 2;
+        private int[] data;
+        public int Count { get; private set; }
+        public MyList()
+            : this(InitialLenght)
+        {
+        }
+        public MyList(int capacity)
+        {
+            this.capacity = capacity;
+            this.data = new int[capacity];
+            this.Count = 0;
+        }
+
+        private void ValidateIndex(int index)
+        {
+
+            if (index >= 0 && index < this.Count)
+            {
+                return;
+            }
+            var message = this.Count == 0
+                ? "This list is empty"
+                : $"This list has {this.Count} elements and it's zero-based, you are trying to access {index} index which is not in the list.";
+            throw new Exception($"Index out of range. {message}");
+        }
+        public int this[int index]
+        {
+            get
+            {
+                this.ValidateIndex(index);
+                return data[index];
+            }
+            set
+            {
+                this.ValidateIndex(index);
+                data[index] = value;
+            }
+        }
+        private void Resize()
+        {
+            var oldArray = this.data;
+            this.capacity *= 2;
+            var newArray = new int[this.capacity];
+            for (int i = 0; i < oldArray.Length; i++)
+            {
+                newArray[i] = oldArray[i];
+            }
+            this.data = newArray;
+        }
+        private void Shrink()
+        {
+            var oldArray = this.data;
+            this.capacity /= 2;
+            var newArray = new int[this.capacity];
+
+            for (int i = 0; i < newArray.Length; i++)
+            {
+                newArray[i] = oldArray[i];
+            }
+            this.data = newArray;
+        }
+        private void Shift(int startingIndex)
+        {
+            for (int i = startingIndex; i < this.Count - 1; i++)
+            {
+                this.data[i] = this.data[i + 1];
+            }
+        }
+        private void ShiftToRight(int index)
+        {
+            for (int i = this.Count; i > index; i--)
+            {
+                this.data[i] = this.data[i - 1];
+            }
+        }
+        public override string ToString()
+        {
+            return this.ToString();
+        }
+        public void Reverse()
+        {
+            var reverseCounter = this.Count - 1;
+            var numberOfOperations = this.Count / 2;
+            for (int i = 0; i < this.Count / 2; i++)
+            {
+                var temp = this.data[i];
+                this.data[i] = this.data[reverseCounter];
+                this.data[reverseCounter] = temp;
+                reverseCounter--;
+            }
+        }
+        public int Find(int item)
+        {
+            var result = 0;
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (item == this.data[i])
+                    result = i;
+            }
+
+            return result;
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            this.ValidateIndex(firstIndex);
+            this.ValidateIndex(secondIndex);
+            var swapper = this.data[firstIndex];
+            this.data[firstIndex] = this.data[secondIndex];
+            this.data[secondIndex] = swapper;
+        }
+        public bool Contains(int item)
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this.data[i] == item)
+                {
+                    return true;
+                }
+            }
+
+            return false; ;
+        }
+        public void Insert(int index, int item)
+        {
+            this.ValidateIndex(index);
+
+            if (this.Count + 1 == this.data.Length)
+                this.Resize();
+
+            this.ShiftToRight(index);
+            this.data[index] = item;
+
+        }
+        public void Add(int number)
+        {
+            if (this.Count + 1 == this.data.Length)
+                this.Resize();
+            data[this.Count] = number;
+            this.Count++;
+        }
+        public int RemoveAt(int index)
+        {
+            this.ValidateIndex(index);
+
+            var removedElement = data[index];
+            data[index] = default;
+            this.Count--;
+            this.Shift(index);
+            if (this.Count <= this.data.Length / 4)
+                this.Shrink();
+
+            return removedElement;
+        }
+        public void Clear()
+        {
+            this.data = new int[InitialLenght];
+            this.capacity = InitialLenght;
+            this.Count = 0;
+        }
+        public int Capacity()
+        {
+            return this.capacity;
+        }
+
+    }
+}
---
+++ Implementing Stack and Queue/Implementing Stack and Queue/StartUp.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Implementing_Stack_and_Queue
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var originalList = new List<int>();
+
+            var list = new MyList();
+
+            Console.WriteLine($"Count before Adding: {list.Count}");
+            list.Add(0);
+            list.Add(1);
+            Console.WriteLine($"Count after adding two numbers in the list: {list.Count}");
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine($"Count after adding over the initial capacity of 2: {list.Count}");
+            list.Add(4);
+            list.Add(5);
+            Console.WriteLine($"Count after one more resize: {list.Count}");
+            Console.WriteLine($"Capacity after the above operations: {list.Capacity()}\n");
+
+            list[0] = 3;
+            Console.WriteLine($"Indexer functionality added - list[0] = 3 : {list[0]}\n");
+
+            Console.WriteLine($"Remove at index \"0\" and return the value:  {list.RemoveAt(0)}\n");
+            Console.WriteLine($"Count after removing at 0 index: {list.Count}");
+            Console.WriteLine($"Capacity after removing at index \"0\": {list.Capacity()} \n");
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+            Console.WriteLine($"Count after removing at index 0 three more times: {list.Count}\n");
+            Console.WriteLine($"List capacity after all the removes: {list.Capacity()}\n");
+
+            Console.WriteLine("List.Clear();");
+            list.Clear();
+            Console.WriteLine($"List.Count : {list.Count}, List.Capacity : {list.Capacity()}");
+            Console.WriteLine($"List.Contains(5) : {list.Contains(5)}");
+            for (int i = 1; i <= 10; i++)// list.Add(0 - 10)
+            {
+                list.Add(i);
+            }
+            Console.WriteLine($"Added 1 - 10 to the list : List.Count - {list.Count}, List.Capacity - {list.Capacity()}");
+            Console.WriteLine($"List.Contains(5) : {list.Contains(5)}");
+            list.Add(11);
+            list.Add(12);
+            list.Add(13);
+            list.Add(14);
+            list.Add(15);
+            list.Add(16);
+            //list.Add(17);
+            Console.WriteLine($"Added 11 - 16 to the list : List.Count - {list.Count}, List.Capacity - {list.Capacity()}");
+            Console.WriteLine($"List.Find(15) - {list.Find(15)}");
+
+            Console.WriteLine("\nList.Clear();\n");
+            list.Clear();
+            list.Add(0);
+            list.Add(1);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Console.WriteLine($"We have 0, 1, 3, 4 and 5 in the list, list[2] == {list[2]} at the moment.");
+            list.Insert(2, 2);
+            Console.WriteLine($"After List.Insert(2, 2) on index 2 of our list we have list[2] == {list[2]} at the moment.");
+            //list.Insert(0, 10);
+            //Console.WriteLine(list[0]);
+            //Console.WriteLine(list[1]);
+
+            Console.WriteLine("\nList.Clear();\n");
+            list.Clear();
+            list.Add(1);
+            list.Add(0);
+            Console.WriteLine($"Before swap : list[0]=={list[0]}, list[1]=={list[1]}");
+            list.Swap(0, 1);
+            Console.WriteLine($"After swap : list[0]=={list[0]}, list[1]=={list[1]}");
+
+            Console.WriteLine("\nList.Clear();\n");
+            list.Clear();
+            for (int i = 0; i < 7; i++)
+                list.Add(i);
+
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[2]);
+            Console.WriteLine(list[3]);
+            Console.WriteLine(list[4]);
+            Console.WriteLine(list[5]);
+            Console.WriteLine(list[6]);
+            Console.WriteLine($"Reverse : ");
+            list.Reverse();
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            Console.WriteLine(list[2]);
+            Console.WriteLine(list[3]);
+            Console.WriteLine(list[4]);
+            Console.WriteLine(list[5]);
+            Console.WriteLine(list[6]);
+            //Console.WriteLine(String.Join(',',list));
+            //foreach(var element in list)
+            //{
+
+            //}
+
+
+
+
+        }
+    }
+}
