2847d6181be01ad968e2baa2d25c4f6862d846dd
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-06-25 09:52:23 +0300

    First problem of the exam done 100/100 :)

---
+++ CSharp Advanced Exam - 25 June 2022/01. Tiles Master/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Tiles_Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Location   Tile area needed
+            //Sink        40
+            //Oven        50
+            //Countertop  60
+            //Wall        70
+
+            var whiteTilesInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var greyTilesInput = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var whiteTilesStack = new Stack<int>(whiteTilesInput);
+            var greyTilesQueue = new Queue<int>(greyTilesInput);
+
+            var locations = new Dictionary<string, int>();
+
+            while (whiteTilesStack.Any() && greyTilesQueue.Any())
+            {
+                var currentWhite = whiteTilesStack.Peek();
+                var currentGray = greyTilesQueue.Peek();
+                var sumOfTiles = currentWhite + currentGray;
+                var location = string.Empty;
+                var isUsed = false;
+                if (currentWhite != currentGray)
+                {
+                    whiteTilesStack.Pop();
+                    greyTilesQueue.Dequeue();
+                    currentWhite /= 2;
+                    whiteTilesStack.Push(currentWhite);
+                    greyTilesQueue.Enqueue(currentGray);
+                }
+                else if (sumOfTiles == 40)//Sink
+                {
+                    whiteTilesStack.Pop();
+                    greyTilesQueue.Dequeue();
+                    location = "Sink";
+                    isUsed = true;
+                }
+                else if (sumOfTiles == 50)//Oven
+                {
+                    whiteTilesStack.Pop();
+                    greyTilesQueue.Dequeue();
+                    location = "Oven";
+                    isUsed = true;
+                }
+                else if (sumOfTiles == 60)//Countertop
+                {
+                    whiteTilesStack.Pop();
+                    greyTilesQueue.Dequeue();
+                    location = "Countertop";
+                    isUsed = true;
+                }
+                else if (sumOfTiles == 70)//Wall
+                {
+                    whiteTilesStack.Pop();
+                    greyTilesQueue.Dequeue();
+                    location = "Wall";
+                    isUsed = true;
+                }
+                else//Floor
+                {
+                    whiteTilesStack.Pop();
+                    greyTilesQueue.Dequeue();
+                    location = "Floor";
+                    isUsed = true;
+                }
+
+                if (isUsed)
+                {
+                    if (!locations.ContainsKey(location))
+                    {
+                        locations.Add(location, 1);
+                    }
+                    else
+                    {
+                        locations[location]++;
+                    }
+                }
+            }
+
+            var whiteTilesLeft = whiteTilesStack.Count == 0 ? "none" : String.Join(", ", whiteTilesStack);
+            var greyTilesLeft = greyTilesQueue.Count == 0 ? "none" : String.Join(", ", greyTilesQueue);
+            Console.WriteLine($"White tiles left: {whiteTilesLeft}");
+            Console.WriteLine($"Grey tiles left: {greyTilesLeft}");
+
+            foreach (var item in locations.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ CSharp-Advanced Exam - 20 February 2022/02. Beaver at Work/BeaverWork.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Beaver_at_Work
+{
+    public class BeaverWork
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var pond = new char[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(char.Parse).ToArray();
+                for (int x = 0; x < n; x++)
+                {
+                    pond[i, x] = line[x];
+                }
+            }
+            var branches = new List<char>();
+            while (true)
+            {
+                var coordinates = BeaverCoordinates(pond, n);
+                var row = coordinates[0];
+                var col = coordinates[1];
+                string cmd = Console.ReadLine();
+                char nextStep = default;
+                int[] nextStepCoordinates = new int[2];
+                nextStepCoordinates[0] = row;
+                nextStepCoordinates[1] = col;
+                switch (cmd)
+                {
+                    case "up":
+                        if (row == 0)
+                            if (branches.Count > 0)
+                                branches.RemoveAt(branches.Count - 1);
+                            else
+                            {
+                                nextStepCoordinates[0] = row - 1;
+                                nextStep = pond[row - 1, col];
+                            }
+                        break;
+                    case "down":
+                        if (row == n - 1)
+                            if (branches.Count > 0)
+                                branches.RemoveAt(branches.Count - 1);
+                            else
+                                nextStep = pond[row + 1, col];
+                        break;
+                    case "left":
+                        if (col == 0)
+                            if (branches.Count > 0)
+                                branches.RemoveAt(branches.Count - 1);
+                            else
+                                nextStep = pond[row, col - 1];
+                        break;
+                    case "right":
+                        if (col == n - 1)
+                            if (branches.Count > 0)
+                                branches.RemoveAt(branches.Count - 1);
+                            else
+                                nextStep = pond[row, col + 1];
+                        break;
+                }
+                if(char.IsLetter(nextStep))
+                    if(char.IsLower)
+            }
+
+
+        }
+        public static int[] BeaverCoordinates(char[,] pond, int size)
+        {
+            var result = new int[2];
+            for (int i = 0; i < size; i++)
+            {
+                for (int x = 0; x < size; x++)
+                {
+                    if (pond[i, x] == 'B')
+                    {
+                        result[0] = i;
+                        result[1] = x;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
