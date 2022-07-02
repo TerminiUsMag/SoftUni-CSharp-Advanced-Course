70a048700161c6fb6a49ae6be9848fb1a156dc71
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-07-02 15:56:17 +0300

    Making a demo to understand IComparable and IComparer.

--- Iterators and Comparators - Lab/Iterators and Comparators - Lab/Demo.cs
+++ Iterators and Comparators - Lab/Iterators and Comparators - Lab/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Iterators_and_Comparators___Lab
 {
@@ -6,7 +7,32 @@ namespace Iterators_and_Comparators___Lab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstPile = new PileOfCash();
+            var secondPile = new PileOfCash();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "Compare")
+            {
+                var cmdTokens = cmd.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                Console.WriteLine($"Add notes to which Pile? ");
+                var pile = Console.ReadLine();
+                pile = pile.ToLower();
+                var type = cmdTokens[0];
+                var number = int.Parse(cmdTokens[1]);
+                if(pile == "first")
+                {
+                    firstPile.AddNotes(type, number);
+                }
+                else if (pile == "second")
+                {
+                    secondPile.AddNotes(type, number);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect pile to add to, try again!");
+                }
+            }
+
+            Console.WriteLine(firstPile.CompareTo(secondPile));
         }
     }
 }
---
+++ Iterators and Comparators - Lab/Iterators and Comparators - Lab/PileOfCash.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Iterators_and_Comparators___Lab
+{
+    class PileOfCash : IComparable<PileOfCash>
+    {
+        private double value;
+        public Dictionary<string, int> Notes { get; set; }
+
+        public PileOfCash()
+        {
+            Notes = new Dictionary<string, int>();
+        }
+        public void AddNotes(string typeOfNote, int numberOfNotes)
+        {
+            if (!Notes.ContainsKey(typeOfNote))
+            {
+                Notes.Add(typeOfNote, numberOfNotes);
+            }
+            else
+            {
+                Notes[typeOfNote]+=numberOfNotes;
+            }
+
+            foreach(var note in this.Notes)
+            {
+                var type = int.Parse(note.Key);
+                var number = note.Value;
+
+                this.value += (type * number);
+            }
+        }
+        public void Reset()
+        {
+            this.Notes.Clear();
+            this.value = 0;
+        }
+
+        public int CompareTo(PileOfCash other)
+        {
+            var result = this.value.CompareTo(other.value);
+            return result;
+        }
+    }
+}
