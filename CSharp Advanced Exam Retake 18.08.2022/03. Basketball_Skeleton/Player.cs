eb3e15a62d67affdcc82afbf51adbf4bb681f2b1
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-08-30 13:16:23 +0300

    Added files for 03.Basketball

---
+++ CSharp Advanced Exam Retake 18.08.2022/03. Basketball_Skeleton/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Basketball
+{
+    public class Player
+    {
+        private bool retired;
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double Rating { get; set; }
+        public int Games { get; set; }
+        public bool Retired
+        {
+            get => retired;
+            set => retired = value;
+        }
+        public Player(string name, string position, double rating, int games)
+        {
+            this.Name = name;
+            this.Position = position;
+            this.Rating = rating;
+            this.Games = games;
+            this.Retired = false;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"-Player: {this.Name}");
+            sb.AppendLine($"--Position: {this.Position}");
+            sb.AppendLine($"--Rating: {this.Rating}");
+            sb.AppendLine($"--Games played: {this.Games}");
+
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ CSharp Advanced Exam Retake 18.08.2022/03. Basketball_Skeleton/Team.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Basketball
+{
+    public class Team
+    {
+        private Dictionary<string, Player> players;
+        private int count;
+
+        public string Name { get; set; }
+        public int OpenPositions { get; set; }
+        public char Group { get; set; }
+        public int Count
+        {
+            get => players.Count;
+        }
+
+        public Team(string name, int openPositions, char Group)
+        {
+            this.players = new Dictionary<string, Player>();
+            this.Name = name;
+            this.OpenPositions = openPositions;
+            this.Group = Group;
+        }
+
+        public string AddPlayer(Player player)
+        {
+            if (string.IsNullOrEmpty(player.Name) || string.IsNullOrEmpty(player.Position))
+                return "Invalid player's information.";
+            else if (this.OpenPositions <= 0)
+                return "There are no more open positions.";
+            else if (player.Rating < 80)
+                return "Invalid player's rating.";
+            players.Add(player.Name, player);
+            this.OpenPositions--;
+            return $"Successfully added {player.Name} to the team. Remaining open positions: {this.OpenPositions}.";
+        }
+        public bool RemovePlayer(string name)
+        {
+            if (players.ContainsKey(name))
+            {
+                players.Remove(name);
+                this.OpenPositions++;
+                return true;
+            }
+            return false;
+        }
+        public int RemovePlayerByPosition(string position)
+        {
+            var listOfPlayerNamesToRemove = new List<string>();
+            foreach (var player in players)
+            {
+                if (player.Value.Position == position)
+                {
+                    listOfPlayerNamesToRemove.Add(player.Value.Name);
+                }
+            }
+            if (listOfPlayerNamesToRemove.Count > 0)
+            {
+                foreach (var name in listOfPlayerNamesToRemove)
+                {
+                    RemovePlayer(name);
+                    
+                }
+                return listOfPlayerNamesToRemove.Count;
+            }
+            return 0;
+        }
+        public Player RetirePlayer(string name)
+        {
+            if (players.ContainsKey(name))
+            {
+                players[name].Retired = true;
+                return players[name];
+            }
+            return null;
+        }
+        public List<Player> AwardPlayers(int games)
+        {
+            var result = new List<Player>();
+            foreach (var player in players)
+            {
+                if (player.Value.Games >= games)
+                {
+                    result.Add(player.Value);
+                }
+            }
+            return result;
+        }
+        public string Report()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Active players competing for Team {this.Name} from Group {this.Group}:");
+            foreach (var player in players.Where(pl=>!pl.Value.Retired))
+            {
+                sb.AppendLine(player.Value.ToString());
+            }
+            return sb.ToString().Trim();
+        }
+
+
+    }
+}
