3e97020066cc4ac81b35e1fe854a58d1d5302181
TerminiUsMag <59938500+TerminiUsMag@users.noreply.github.com>
2022-08-30 12:33:21 +0300

    Added files from C# Advanced EXAM RETAKE 18.08.2022

---
+++ CSharp Advanced Exam Retake 18.08.2022/BaristaContest/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BaristaContest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffee = new Queue<int>();
+            var milk = new Stack<int>();
+            var madeDrinks = new Dictionary<string, int>();
+
+            var coffeeInput = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var milkInput = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            for (int i = 0; i < coffeeInput.Length; i++)
+            {
+                coffee.Enqueue(coffeeInput[i]);
+            }
+            for (int y = 0; y < milkInput.Length; y++)
+            {
+                milk.Push(milkInput[y]);
+            }
+
+            while (coffee.Count > 0 && milk.Count > 0)
+            {
+                var sum = coffee.Peek() + milk.Peek();
+                switch (sum)
+                {
+                    case 50://Cortado
+                        coffee.Dequeue();
+                        milk.Pop();
+                        AddToDict(madeDrinks, "Cortado");
+
+                        break;
+                    case 75://Espresso
+
+                        coffee.Dequeue();
+                        milk.Pop();
+                        AddToDict(madeDrinks, "Espresso");
+
+                        break;
+                    case 100://Capuccino
+
+                        coffee.Dequeue();
+                        milk.Pop();
+                        AddToDict(madeDrinks, "Capuccino");
+
+                        break;
+                    case 150://Americano
+
+                        coffee.Dequeue();
+                        milk.Pop();
+                        AddToDict(madeDrinks, "Americano");
+
+                        break;
+                    case 200://Latte
+
+                        coffee.Dequeue();
+                        milk.Pop();
+                        AddToDict(madeDrinks, "Latte");
+
+                        break;
+                    default://None
+
+                        coffee.Dequeue();
+                        var milkToDecrease = milk.Pop();
+                        milkToDecrease -= 5;
+                        milk.Push(milkToDecrease);
+                        //var tempMilk = new Stack<int>();
+                        //for (int x = 0; x < milk.Count; x++)
+                        //{
+                        //    tempMilk.Push(milk.Pop());
+                        //}
+
+                        break;
+                }
+            }
+
+            if (coffee.Count > 0 || milk.Count > 0)
+            {
+                Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+            }
+            else
+            {
+                Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+            }
+
+            var coffeeLeft = coffee.Count == 0 ? "none" : String.Join(", ", coffee);
+            var milkLeft = milk.Count == 0 ? "none" : String.Join(", ", milk);
+
+            Console.WriteLine($"Coffee left: {coffeeLeft}");
+            Console.WriteLine($"Milk left: {milkLeft}");
+
+            var orderedDrinksDict = madeDrinks.OrderBy(d => d.Value).ThenByDescending(d => d.Key);
+
+            foreach (var drink in orderedDrinksDict)
+            {
+                Console.WriteLine($"{drink.Key}: {drink.Value}");
+            }
+
+        }
+        public static void AddToDict(Dictionary<string, int> dict, string key)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                dict.Add(key, 1);
+            }
+            else
+            {
+                dict[key]++;
+            }
+        }
+    }
+}
---
+++ CSharp Advanced Exam Retake 18.08.2022/HelpAMole/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace HelpAMole
+{
+    internal class Program
+    {
+        static int totalPoints = 0;
+        static void Main(string[] args)
+        {
+            int n;
+            while ((n = int.Parse(Console.ReadLine())) < 2 || n > 10)
+            { }
+
+            var wall = new char[n, n];
+            wall = EnterRows(n, wall);
+            int[] mPosition = DetermineMPosition(n, wall);
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "End" && totalPoints < 25)
+            {
+                wall = Move(n, wall, mPosition, cmd);
+                mPosition = DetermineMPosition(n, wall);
+            }
+            if (totalPoints >= 25)
+            {
+                Console.WriteLine("Yay! The Mole survived another game!");
+                Console.WriteLine($"The Mole managed to survive with a total of {totalPoints} points.");
+            }
+            else
+            {
+                Console.WriteLine("Too bad! The Mole lost this battle!");
+                Console.WriteLine($"The Mole lost the game with a total of {totalPoints} points.");
+            }
+            PrintWall(n, wall);
+        }
+
+        public static char[,] Move(int n, char[,] wall, int[] mPosition, string cmd)
+        {
+            var mX = mPosition[0];
+            var mY = mPosition[1];
+            char nextPosValue = ' ';
+            int[] nextPos = new int[2];
+            try
+            {
+                switch (cmd)
+                {
+                    case "up":
+                        nextPosValue = wall[mX - 1, mY];
+                        nextPos[0] = mX - 1;
+                        nextPos[1] = mY;
+                        break;
+                    case "down":
+                        nextPosValue = wall[mX + 1, mY];
+                        nextPos[0] = mX + 1;
+                        nextPos[1] = mY;
+                        break;
+                    case "left":
+                        nextPosValue = wall[mX, mY - 1];
+                        nextPos[0] = mX;
+                        nextPos[1] = mY - 1;
+                        break;
+                    case "right":
+                        nextPosValue = wall[mX, mY + 1];
+                        nextPos[0] = mX;
+                        nextPos[1] = mY + 1;
+                        break;
+                    default:
+                        return wall;
+
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Don't try to escape the playing field!");
+                return wall;
+            }
+            if (nextPosValue == 'S')
+            {
+                wall[nextPos[0], nextPos[1]] = '-';
+                var findSecondS = DetermineSPosition(n, wall);
+                wall[findSecondS[0], findSecondS[1]] = 'M';
+                totalPoints -= 3;
+
+                //if (fmSuccess)
+                //    wall[mX, mY] = '*';
+                //else
+                //{
+                //    wall[mX, mY] = '*';
+                //    countOfHoles++;
+                //}
+                //Console.WriteLine($"Vanko got electrocuted, but he managed to make {countOfHoles} hole(s).");
+                //PrintWall(n, wall);
+
+                //return null;
+
+            }
+            else if (nextPosValue == '-')
+            {
+                wall[nextPos[0], nextPos[1]] = 'M';
+            }
+            //else if (nextPosValue == '*')
+            //{
+            //    Console.WriteLine($"The wall is already destroyed at position [{nextPos[0]}, {nextPos[1]}]!");
+            //    wall[nextPos[0], nextPos[1]] = 'V';
+            //    wall[mX, mY] = '*';
+            //}
+            else
+            {
+                var pointsToCollect = nextPosValue - '0';
+                wall[nextPos[0], nextPos[1]] = 'M';
+                totalPoints += pointsToCollect;
+            }
+            wall[mX, mY] = '-';
+            return wall;
+        }
+
+        public static void PrintWall(int n, char[,] wall)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int x = 0; x < n; x++)
+                {
+                    Console.Write(wall[i, x]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static char[,] EnterRows(int numberOfRows, char[,] wall)
+        {
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                string currRow = Console.ReadLine();
+                for (int j = 0; j < numberOfRows; j++)
+                {
+                    if (currRow.Length > j)
+                        wall[i, j] = currRow[j];
+                    else wall[i, j] = '-';
+                }
+            }
+            return wall;
+        }
+        public static int[] DetermineMPosition(int numberOfRows, char[,] wall)
+        {
+            int[] mPosition = new int[2];
+            if (wall[mPosition[0], mPosition[1]] != 'M')
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    for (int x = 0; x < numberOfRows; x++)
+                    {
+                        if (wall[i, x] == 'M')
+                        {
+                            mPosition[0] = i;
+                            mPosition[1] = x;
+                        }
+                    }
+                }
+            return mPosition;
+        }
+        public static int[] DetermineSPosition(int numberOfRows, char[,] wall)
+        {
+            int[] sPosition = new int[2];
+            if (wall[sPosition[0], sPosition[1]] != 'S')
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    for (int x = 0; x < numberOfRows; x++)
+                    {
+                        if (wall[i, x] == 'S')
+                        {
+                            sPosition[0] = i;
+                            sPosition[1] = x;
+                        }
+                    }
+                }
+            return sPosition;
+        }
+    }
+}
